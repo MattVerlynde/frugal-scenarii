0c87f953288781dd572166706b577812c7d71ad6
Matthieu <matthieu.verlynde@agroparistech.fr>
2025-08-04 11:03:02 +0200

    Automatic commit before running experiment conso-imagenet:

--- classif_torch.py
+++ classif_torch.py
@@ -268,7 +268,7 @@ if __name__ == "__main__":
     pretransform = CustomCompose([
         transforms.Resize(img_size, interpolation=transforms.InterpolationMode.BILINEAR),  # Resize the image to the specified size
         transforms.CenterCrop(crop_size),  # Center crop the image to the specified size
-        transforms.ToTensor(),           # Convert the PIL image to a tensor
+        # transforms.ToTensor(),           # Convert the PIL image to a tensor
         transforms.Normalize(
             mean=[0.485, 0.456, 0.406], 
             std=[0.229, 0.224, 0.225])  # Normalize the image
@@ -277,7 +277,7 @@ if __name__ == "__main__":
     transform = CustomCompose([
         transforms.Resize(img_size, interpolation=transforms.InterpolationMode.BILINEAR),  # Resize the image to the specified size
         transforms.CenterCrop(crop_size),  # Center crop the image to the specified size
-        transforms.ToTensor(),           # Convert the PIL image to a tensor
+        # transforms.ToTensor(),           # Convert the PIL image to a tensor
         transforms.Normalize(
             mean=[0.485, 0.456, 0.406], 
             std=[0.229, 0.224, 0.225])  # Normalize the image
