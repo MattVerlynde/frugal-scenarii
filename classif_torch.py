9eecc66f2d309c61927caf2d4cbae0baa7d5aece
Matthieu <matthieu.verlynde@agroparistech.fr>
2025-08-25 13:18:14 +0200

    Automatic commit before running experiment conso-imagenet:

--- classif_torch.py
+++ classif_torch.py
@@ -86,7 +86,8 @@ def early_stopping(train_loss, val_loss, min_delta, patience, counter=0):
 def train(trainloader, validloader, model, num_epochs=100, transform=None, seed=42):
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(model.parameters(), lr=0.01)
+    # optimizer = optim.SGD(model.parameters(), lr=0.01)
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
     scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=30, gamma=0.1)
     # scheduler = optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.9)
 
